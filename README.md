82fb4fbb2cc96b8c127c41ba0c64a634a7ee9375
simplysabir <simplysabir@gmail.com>
2023-02-02 08:42:10 +0530

    Project Completed

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Check Solana Balance  
+
+Enter your Solana Wallet address and it will tell you How Much Balance does the wallet have and is it executable or not.  
 
 ## Getting Started
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import AddressForm from '../components/AddressForm'
-import * as Web3 from '@solana/web3.js'
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
+import AddressForm from '../components/AddressForm';
+import * as Web3 from '@solana/web3.js';
+import "cal-sans";
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
     try {
@@ -15,6 +17,9 @@ const Home: NextPage = () => {
       const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      })
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
       })
     } catch (error) {
       setAddress('')
@@ -27,11 +32,12 @@ const Home: NextPage = () => {
     <div className={styles.App}>
       <header className={styles.AppHeader}>
         <p>
-          Start Your Solana Journey
+          Enter Your Solana Address 
         </p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   )
